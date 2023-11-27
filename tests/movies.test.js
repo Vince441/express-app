fb753b70ee7cc2535796fd8797e3b035eb477652
vincentlouvart82@gmail.com <Vinc441>
2023-11-27 17:10:53 +0100

    ajout describe GET /api/movies/:id

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -11,6 +11,10 @@ expect(response.status).toEqual(200);
 
 });
 
+});
+
+describe("GET /api/movies/:id", () =>{
+
 it("should return all movies", async () =>{
     const response = await request(app).get("/api/movies/1");
     expect(response.headers["content-type"]).toMatch(/json/);
@@ -18,12 +22,17 @@ it("should return all movies", async () =>{
     
     });
 
+
+});
+
+describe("GET /api/movies/:id", () =>{
+
     it("should return all movies", async () =>{
         const response = await request(app).get("/api/movies/0");
         expect(response.status).toEqual(404);
         
         });
 
+    });
 
-});
 
