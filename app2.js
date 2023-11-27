a12a861845395dda7a10942d9198cd450514b673
vincentlouvart82@gmail.com <Vinc441>
2023-11-27 10:13:36 +0100

    test des status avec superTest et jest

---
+++ app2.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5001;
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
